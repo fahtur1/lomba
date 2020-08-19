46bfaa3328f5134b037c042c567ba87780ea2d6d
fahtur1 <fahturrahman18ti@mahasiswa.pcr.ac.id>
2020-08-20 02:47:03 +0700

    add a few models

--- application/models/Plan_Model.php
+++ application/models/Plan_Model.php
@@ -1,35 +1,37 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Product_Model extends CI_Model
+class Plan_Model extends CI_Model
 {
-    private $table = 'product';
-    private $key = 'product_id';
+    private $table = 'plan';
+    private $key = 'plan_id';
 
-    public function insertProduct($data)
+    public function insertPlan($data)
     {
         return $this->db->insert($this->table, $data);
     }
 
-    public function updateProduct($data)
+    public function updatePlan($data)
     {
         $this->db->where($this->key, $data[$this->key]);
         return $this->db->update($this->table, $data);
     }
 
-    public function getProducts()
+    public function getPlans()
     {
         $this->db->select('*');
         $this->db->from($this->table);
-        $this->db->join('model', 'model.model_id = product.model_id');
+        $this->db->join('product', 'product.product_id = plan.product_id');
+        $this->db->join('model', 'model.product_id = product.product_id');
         return $this->db->get()->result_array();
     }
 
-    public function getProductById($id)
+    public function getPlanById($id)
     {
         $this->db->select('*');
         $this->db->from($this->table);
-        $this->db->join('model', 'model.model_id = product.model_id');
+        $this->db->join('product', 'product.product_id = plan.product_id');
+        $this->db->join('model', 'model.product_id = product.product_id');
         $this->db->where($this->key, $id);
         return $this->db->get()->row_array();
     }
