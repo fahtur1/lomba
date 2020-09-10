4acb1d84d5ed14988eefb2eb056bd2737678de1f
Fahturrahman Fauzi <fahturrahmanfauzi@pop-os.localdomain>
2020-09-10 22:33:01 +0700

    ganti url 404

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -20,7 +20,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			<div class="error mx-auto" data-text="404">404</div>
 			<p class="lead text-gray-800 mb-5">Page Not Found</p>
 			<p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-			<a href="http://utppmbisa.com/admin/home/dashboard">← Back to Dashboard</a>
+			<a href="http://utppmbisa.com/Admin/Home/dashboard">← Back to Dashboard</a>
 		</div>
 
 	</div>
