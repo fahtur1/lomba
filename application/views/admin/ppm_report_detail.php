85d85ee81bad03e77a6d681f415ca485a08b3563
ArdyJunata <ardyjunata53@gmail.com>
2020-09-11 00:15:01 +0700

    naming

--- application/views/admin/ppm_report_detail.php
+++ application/views/admin/ppm_report_detail.php
@@ -1,4 +1,4 @@
-<div class="container-fluid" id="content">
+<div class="container-fluid" id="content" name="content">
     <div class="row">
         <div class="col-lg-3">
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
