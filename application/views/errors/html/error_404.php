f232f551a9b25777a0d184b2b80e947f12e492e6
ArdyJunata <ardyjunata53@gmail.com>
2020-09-10 20:56:28 +0700

    remove base url

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -5,11 +5,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html lang="en">
 
 <head>
-	<link href="<?= base_url('assets') ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="http://localhost/lomba/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
 	<!-- Custom styles for this template-->
-	<link href="<?= base_url('assets') ?>/css/sb-admin-2.min.css" rel="stylesheet">
+	<link href="http://localhost/lomba/assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -20,7 +20,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			<div class="error mx-auto" data-text="404">404</div>
 			<p class="lead text-gray-800 mb-5">Page Not Found</p>
 			<p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-			<a href="<?= base_url() ?>/admin/home/dashboard">← Back to Dashboard</a>
+			<a href="http://localhost/lomba/admin/home/dashboard">← Back to Dashboard</a>
 		</div>
 
 	</div>
